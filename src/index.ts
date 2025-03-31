ce794490acffcc2c4e2ac64d3fdbd63e361bef0c
gitcode <website0wordpress@gmail.com>
2025-03-31 11:20:58 +0530

    vo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
-const bootstrap = () => {
-  console.log(`🥳 Welcome to use the CLI Unbuild Template!`);
-};
 
-bootstrap();
+import { Command } from "commander";
+import yoctoSpinner from "yocto-spinner";
+
+async function main() {
+  const program = new Command("better-auth");
+
+  program.action(() => console.log("hello world !!"));
+  program.description("Better Auth CLI");
+
+  const spinner = yoctoSpinner({ text: "preparing schema..." }).start();
+
+  setTimeout(() => {
+    spinner.success("Success!");
+  }, 2000);
+
+  program.parse();
+}
+
+main();
